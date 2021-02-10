6fe91e3cf91121874a6e525707514594640fbfe8
Dennynic <joragdetibil@gmail.com>
2021-02-10 09:22:59 +0500

    Add to project graphql

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,21 +1,44 @@
 import React from "react";
+import axios from "axios";
 
 class Portfolios extends React.Component {
-  static getInitialProps() {
-    return { testingData: "Some testing Data 111" };
+  static async getInitialProps() {
+    const portfolios = await this.onFetchPortfolios();
+    return { portfolios };
+  }
+
+  static onFetchPortfolios() {
+    const query = `
+      query Portfolios {
+        portfolios {
+          _id, 
+          title, 
+          company, 
+          companyWebsite,
+          location,
+          jobTitle,
+          description
+         }
+       }`;
+    return axios
+      .post("http://localhost:3000/graphql", { query })
+      .then(({ data: graph }) => graph.data)
+      .then((data) => data.portfolios);
   }
 
   render() {
-    const { testingData } = this.props;
+    const { portfolios } = this.props;
+
     return (
       <>
         <section className="section-title">
           <div className="px-2">
             <div className="pt-5 pb-4">
-              <h1>Portfolios {testingData}</h1>
+              <h1>Portfolios</h1>
             </div>
           </div>
         </section>
+        {JSON.stringify(portfolios)}
         <section className="pb-5">
           <div className="row">
             <div className="col-md-4">
